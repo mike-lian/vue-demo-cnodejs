321d6cd0e193cddec8db28f9a57ca25fb361abae
lianwei <422510444@qq.com>
2018-07-05 11:40:36 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="ap">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="scss">
+  @import "./style/style"
+</style>
+
---
+++ src/page/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <Header></Header>
+        <div class="article_list">
+            <ul>
+                <li v-for="i in list">
+                    <time v-text="$utils.goodTime(i.create_at)"></time>  
+                    <router-link :to="'/content/'+ i.id">
+                        {{i.title}}
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from '../components/header'
+import Footer from '../components/footer'
+export default {
+    components: {
+        Header,Footer
+    },
+    data() {
+        return {
+            list: []
+        }
+    },
+    created() {
+        this.getData();
+        console.log(this.$route)
+    },
+    methods: {
+        getData() {
+            this.$api.get('topics',null,r=>{
+                this.list = r.data
+            })
+        }
+    }
+}
+</script>
+
